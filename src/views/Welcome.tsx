217ac23681ef9a2899c9825b8dc96e9182ec4f88
aliyu <1324149139@qq.com>
2022-10-24 18:20:22 +0800

    滑动切换路由

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,11 +1,12 @@
-import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue';
-import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
-import { useSwipe } from '../hooks/useSwipe';
+import {defineComponent, ref, Transition, VNode, watchEffect} from 'vue';
+import {RouteLocationNormalizedLoaded, RouterView} from 'vue-router';
+import {useSwipe} from '../hooks/useSwipe';
 import s from './Welcome.module.scss'
+
 export const Welcome = defineComponent({
   setup: (props, context) => {
-    const main = ref<HTMLElement | null>(null)
-    const { direction, swiping } = useSwipe(main)
+    const main = ref<HTMLElement>()
+    const {direction, swiping} = useSwipe(main)
     watchEffect(() => {
       console.log(swiping.value, direction.value)
     })
@@ -18,16 +19,16 @@ export const Welcome = defineComponent({
       </header>
       <main class={s.main} ref={main}>
         <RouterView name="main">
-          {({ Component: X, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
+          {({Component: X, route: R}: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
             <Transition enterFromClass={s.slide_fade_enter_from} enterActiveClass={s.slide_fade_enter_active}
-              leaveToClass={s.slide_fade_leave_to} leaveActiveClass={s.slide_fade_leave_active}>
+                        leaveToClass={s.slide_fade_leave_to} leaveActiveClass={s.slide_fade_leave_active}>
               {X}
             </Transition>
           }
         </RouterView>
       </main>
       <footer>
-        <RouterView name="footer" />
+        <RouterView name="footer"/>
       </footer>
     </div>
   }
